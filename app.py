6db419bbde5ac013f6d0c4a03c8206db8a6818e1
xann <77863912+xannbtw@users.noreply.github.com>
2024-05-22 18:15:22 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -2,34 +2,38 @@ from flask import Flask, request, redirect, url_for, send_file, render_template
 import yt_dlp as youtube_dl  # Importar yt-dlp en lugar de youtube_dl
 import os
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def crear_app():
+    app = Flask(__name__)
 
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form['url']
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'outtmpl': 'downloads/%(title)s.%(ext)s',
-        'noplaylist': True,
-    }
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info_dict)
-        filename = os.path.splitext(filename)[0] + ".mp3"
+    @app.route('/download', methods=['POST'])
+    def download():
+        url = request.form['url']
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            'noplaylist': True,
+        }
 
-    return send_file(filename, as_attachment=True)
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info_dict)
+            filename = os.path.splitext(filename)[0] + ".mp3"
+
+        return send_file(filename, as_attachment=True)
+    return app
 
 if __name__ == '__main__':
     if not os.path.exists('downloads'):
         os.makedirs('downloads')
+    app = crear_app()
     app.run(debug=True)
